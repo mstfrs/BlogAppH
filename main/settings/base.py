d354fdd38ef675a4a37aa6789c44e7b56c2d3a2b
mstfrs <mstfrs68@gmail.com>
2022-03-17 23:59:48 +0300

    allowed hosts

--- main/settings/base.py
+++ main/settings/base.py
@@ -28,7 +28,8 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ['mors-blogapp.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://mors-blogapp.herokuapp.com/',
+                 'localhost', '127.0.0.1']
 
 
 # Application definition
