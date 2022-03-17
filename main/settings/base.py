77644eeff9c10b946b859ce65af567a962174652
mstfrs <mstfrs68@gmail.com>
2022-03-17 23:43:12 +0300

    heroku settings

--- main/settings/base.py
+++ main/settings/base.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mors-blogapp.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
