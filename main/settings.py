b603c4ca54a881da83131a735389b9124eec41c1
mstfrs <mstfrs68@gmail.com>
2022-03-24 00:02:27 +0300

    cors

--- main/settings.py
+++ main/settings.py
@@ -178,3 +178,10 @@ CORS_ALLOW_METHODS = [
     "POST",
     "PUT",
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+]
